406ab3797fe3fef2c1a83b30b870e21fba52b831
JayCeM <johann.carl@online.de>
2021-05-28 20:20:26 +0200

    removed hello world code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use std::env;
 fn main() {
-    println!("Hello, world!");
-    let mut arr = [0, 1, 3];
-    arr[1] = 5;
-    println!("{:?}", arr);
     let mut args = env::args().skip(1);
     match args.next() {
         Some(s) => {
